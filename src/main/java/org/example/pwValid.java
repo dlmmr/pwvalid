926dad13c4468f9181c622f020eb9de1aa7afd8d
Dennis Lemmer <dlemmer28@gmail.com>
2025-08-28 11:17:31 +0200

    added class pwValid with first methods

---
+++ src/main/java/org/example/pwValid.java
@@ -0,0 +1,39 @@
+package org.example;
+
+public class pwValid {
+    public static void main(String[] args) {
+
+    }
+
+    public static boolean isNull(String password) {
+        return false;
+    }
+
+    public static boolean isBlank(String password) {
+        return false;
+    }
+
+    public static boolean hasMinLength(String password, int min) {
+        return false;
+    }
+
+    public static boolean containsDigit(String password) {
+        return false;
+    }
+
+    public static boolean containsUpperAndLower(String password) {
+        return false;
+    }
+
+    public static boolean containsSpecialChar(String password, String allowed) {
+        return false;
+    }
+
+    public static boolean hasMinGroups(String password, int requiredGroups) {
+        return false;
+    }
+
+    public static boolean isCommonPassword(String password) {
+        return false;
+    }
+}
